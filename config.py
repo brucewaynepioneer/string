f1e6769e5c07013a567f19860f6bfbbe785ff98c
brucewaynepioneer <158065590+brucewaynepioneer@users.noreply.github.com>
2024-03-06 00:14:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,5 +9,5 @@ API_HASH = "1fda88a5d1de46058a4791c78bce198e"
 BOT_TOKEN = "6670856824:AAEeDwlO0oFJcj2rjrl35luR2uRw23XB73E"
 OWNER_ID = "6441456023"
 
-MONGO_DB_URI = "mongodb+srv://alreadydead:<0nQgs6xZaoXTnEbm>@cluster0.b6weprc.mongodb.net/?retryWrites=true&w=majority"
+MONGO_DB_URI = "mongodb+srv://spymusicbot:spymusicbot@cluster0.l4pi5sr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 MUST_JOIN = "stringsessionAK47"
