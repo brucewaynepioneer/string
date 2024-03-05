10e22b3cbcce7d0960ebbbf0d34e76db2a854488
brucewaynepioneer <158065590+brucewaynepioneer@users.noreply.github.com>
2024-03-06 00:46:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 API_ID = "26075120"
 API_HASH = "1fda88a5d1de46058a4791c78bce198e"
 
-BOT_TOKEN = "6670856824:AAEeDwlO0oFJcj2rjrl35luR2uRw23XB73E"
+BOT_TOKEN = "7006666328:AAHMXefPM9OvRo2wCyy-pH2LqutJ57S-vr4"
 OWNER_ID = "6441456023"
 
 MONGO_DB_URI = "mongodb+srv://spymusicbot:spymusicbot@cluster0.l4pi5sr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
